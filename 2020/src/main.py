b0d99c4cbd0f47e25e4da195faed3324656ae427
Julian Torres <julian.torres@mongodb.com>
2020-12-24 01:03:18 -0500

    Day 1 complete for real this time

--- 2020/src/main.py
+++ 2020/src/main.py
@@ -1,12 +1,17 @@
 input = open("2020/src/input.txt", "r")
+# input_lines = [1721, 979, 366, 299, 675, 1456]
 input_lines = sorted(input.readlines())
+previous_values = []
 
-while len(input_lines) > 0:
-    current_val = int(input_lines.pop(0))
+while len(input_lines) > 1:
+    first_val = int(input_lines[0])
+    previous_values.append(int(input_lines.pop(0)))
 
     for val in input_lines:
         intified_val = int(val)
-        if intified_val + current_val == 2020:
-            print(intified_val, current_val)
-            print(intified_val * current_val)
+
+        for prev_val in previous_values:
+            if prev_val + intified_val + first_val == 2020:
+                print(intified_val, prev_val, first_val)
+                print(intified_val * prev_val * first_val)
 
