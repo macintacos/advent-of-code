dc912bc6893535ca9aacdf0e30695a794f59d57f
Julian Torres <julian.torres@mongodb.com>
2020-12-25 19:55:46 -0500

    Day 3, part 2 - complete

--- 2020/day_03/main.py
+++ 2020/day_03/main.py
@@ -1,24 +1,6 @@
 import math
 from typing import List, Tuple
 
-# Real stuff
-tree_map = open("2020/day_03/input.txt", "r")
-tree_map_list = [line.strip() for line in tree_map.readlines()]
-
-# Test stuff
-# tree_map = """..##.......
-# #...#...#..
-# .#....#..#.
-# ..#.#...#.#
-# .#...##..#.
-# ..#.##.....
-# .#.#.#....#
-# .#........#
-# #.##...#...
-# #...##....#
-# .#..#...#.#"""
-# tree_map_list = tree_map.splitlines()
-
 
 def calculate_dimensions(tree_map_list: List[str]) -> Tuple[int, int]:
     # Make sure we don't have a weird list
@@ -41,11 +23,11 @@ def is_map_final(
     map: List[str], map_dimensions: Tuple[int, int], path: Tuple[int, int]
 ) -> Tuple[bool, int]:
     x_path = path[0]
-    y_path = path[1]  # we don't use this yet, maybe we will in the future?
+    y_path = path[1]
     (map_width, map_height) = map_dimensions
 
     counter = 0
-    for _ in range(map_height):
+    for _ in range(math.ceil(map_height / y_path)):
         counter += x_path
 
     map_copies_to_make = math.ceil(counter / map_width)
@@ -64,18 +46,15 @@ def is_map_final(
 
 def extend_map_for_path(tree_map_list: List[str], path: Tuple[int, int]) -> List[str]:
     dimensions = calculate_dimensions(tree_map_list)
-    print(f"initial dimensions: {dimensions}")
     (is_final, map_copies) = is_map_final(tree_map_list, dimensions, path)
 
     new_map: List[str] = []
 
     if is_final:
-        print("okay final")
         return tree_map_list
     else:
-        print(f"not final yet, copies to make: {map_copies}")
         # Create a new map with the correct width
-        for line in tree_map_list:
+        for i, line in enumerate(tree_map_list):
             new_line = ""
             for _ in range(map_copies):
                 new_line += line
@@ -85,28 +64,35 @@ def extend_map_for_path(tree_map_list: List[str], path: Tuple[int, int]) -> List
 
 
 # Determine the amount of collisions
-def determine_collisions_for_map(valid_map: List[str], path: Tuple[int, int]):
-    print(calculate_dimensions(valid_map))
+def determine_collisions_for_map(valid_map: List[str], path: Tuple[int, int]) -> int:
     counter = 0
     tree_hits = 0
-    move_value = path[0]
+    x_value = path[0]
+    y_value = path[1]
+
     for i, line in enumerate(valid_map):
-        if i == 0:
-            continue
-        counter += move_value
-        if line[counter] == "#":
-            map_marker = "X"
-            tree_hits += 1
-        else:
-            map_marker = "O"
-        print(f"----- Line: {line}")
-        print(f"Iteration: {i}")
-        print(f"Counter: {counter}")
-        print(f"Marker: {map_marker}")
-        print(f"Tree hits: {tree_hits}")
+        if i < y_value:
+            continue  # skip the first iteration
+        if i % y_value == 0:
+            counter += x_value
+            if line[counter] == "#":
+                tree_hits += 1
+
+    print(f"Tree hits for path {path}: {tree_hits}")
+    return tree_hits
 
 
-path_to_traverse = (3, 1)
-new_map = extend_map_for_path(tree_map_list, path_to_traverse)
-determine_collisions_for_map(new_map, path_to_traverse)
+# Do the things
+tree_map = open("2020/day_03/input.txt", "r")
+tree_map_list = [line.strip() for line in tree_map.readlines()]
 
+paths_to_traverse = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+answers: List[int] = []
+
+for path_to_traverse in paths_to_traverse:
+    new_map = extend_map_for_path(tree_map_list, path=path_to_traverse)
+    collisions = determine_collisions_for_map(new_map, path=path_to_traverse)
+    answers.append(collisions)
+
+answer = math.prod(answers)
+print(f"The answer is: {answer}")
