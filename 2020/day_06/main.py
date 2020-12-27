089ccbd96999f9e89b93f79b89d9a5c4e31ea2c0
Julian Torres <julian.torres@mongodb.com>
2020-12-26 22:11:49 -0500

    Day 6, part two - complete

--- 2020/day_06/main.py
+++ 2020/day_06/main.py
@@ -7,16 +7,31 @@ def merge_answers_for_group(group: List[str]) -> Set[str]:
     return merged_answer_set
 
 
+def get_group_answer_intersection(group: List[str]) -> Set[str]:
+    group_sets = map(set, group)
+    merge_answers_for_group = set.intersection(*group_sets)
+
+    return merge_answers_for_group
+
+
 with open("./input.txt", "r") as file:
     content = file.read()
     group_answers = content.split("\n\n")
 
-    num_yes_answers_per_group: List[int] = []
-    for index, group in enumerate(group_answers):
-        # Merge all of the answers into one set of answers
-        # Sum the counts of "yes" answers
+    num_yes_merged_answers_per_group: List[int] = []
+    num_yes_intersection_answers_per_group: List[int] = []
+
+    for group in group_answers:
         group_split = group.splitlines()
         merged_answers = merge_answers_for_group(group_split)
-        num_yes_answers_per_group.append(len(merged_answers))
+        intersection_answers = get_group_answer_intersection(group_split)
 
-    print(f"Number of yes answers in all groups: {sum(num_yes_answers_per_group)}")
+        num_yes_merged_answers_per_group.append(len(merged_answers))
+        num_yes_intersection_answers_per_group.append(len(intersection_answers))
+
+    print(
+        f"Number of yes answers in merged groups: {sum(num_yes_merged_answers_per_group)}"
+    )
+    print(
+        f"Number of yes answers in intersected groups: {sum(num_yes_intersection_answers_per_group)}"
+    )
